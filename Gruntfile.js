8897b26fe893d0eba982eeb4aaa8224ee8f3154c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-02-26 23:54:38 -0800

    heroku ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,16 +246,16 @@ module.exports = function (grunt) {
             dist: {
                 files: [{
                     expand: true,
-                    dot: true,
-                    cwd: '<%= yeoman.app %>',
                     dest: '<%= yeoman.dist %>',
-                    src: [
-                        '*.{ico,txt}',
-                        '.htaccess',
-                        'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*',
-                        'bower_components/sass-bootstrap/fonts/*.*'
-                    ]
+                    cwd: 'heroku',
+                    src: '*',
+                    rename: function (dest, src) {
+                        var path = require('path');
+                        if (src === 'distpackage.json') {
+                            return path.join(dest, 'package.json');
+                        }
+                        return path.join(dest, src);
+                    }
                 }]
             }
         },
